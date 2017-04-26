53fdf7b83d31492f41c947f741ed04a30b48fb5c
mmushfiq <mushfiqazeri@gmail.com>
2017-04-26 15:47:44 +0400

    26.04.2017 herflere color verdim ag reng, nito olsa chixarmaq

--- web/resources/js/index.js
+++ web/resources/js/index.js
@@ -17,30 +17,35 @@
 
 
         var r = 255, b = 255, g = 255;
-        var colors = [];
+        var bgColors = [];
         for (var i = 0; i < 25; i++, b -= 15, g -= 15) {
             if (i < 18) {
-                colors[i] = "rgb(255, " + b + ", " + g + ")";
+                bgColors[i] = "rgb(255, " + b + ", " + g + ")";
             } else {
                 r -= 15;
-                colors[i] = "rgb(" + r + ", 0, 0)";
+                bgColors[i] = "rgb(" + r + ", 0, 0)";
             }
-            console.log('colors[' + i + ']=' + colors[i]);
+            console.log('bgColors[' + i + ']=' + bgColors[i]);
         }
         for (var i = 25; i < 500; i++) {
-            colors[i] = "rgb(150, 0, 0)";
+            bgColors[i] = "rgb(150, 0, 0)";
+        }
+        
+        var colors = [];
+        for (var i = 0; i < 500; i++) {
+            if(i<25) colors[i] = "blue";
+            else colors[i] = "white";
         }
 
         $.get(url, function (data, status) {
-
             for (var x = 0, ln = data.length; x < ln; x++) {
                 setTimeout(function (y) {
-//                    console.log("%d => %d", y, $("#matrix.table tr:eq(" + data[y].row + ") td:eq(" + data[y].column + ")").css("background-color", "red"));
-                    console.log("%d => %d", y, $("#matrix.table tr:eq(" + data[y].row + ") td:eq(" + data[y].column + ")").css("background-color", colors[y]));
+//                    console.log("%d => %d", y, $("#matrix.table tr:eq(" + data[y].row + ") td:eq(" + data[y].column + ")").css("background-color", bgColors[y]));
+                    console.log("%d => %d", y, $("#matrix.table tr:eq(" + data[y].row + ") td:eq(" + data[y].column + ")").css({"background-color": bgColors[y],"color": colors[y]}));
                 }, x * 300, x); // we're passing x
             }
-
         });
+        
     });
 
 })(jQuery);
